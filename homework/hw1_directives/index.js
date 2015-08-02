0d90a2e509d40703b6872b5f4bd871b55a526578
Jared Carrano <jcarrano@newvisions.org>
2015-08-02 12:23:19 -0400

    updated multiple hw1 files

--- homework/hw1_directives/index.js
+++ homework/hw1_directives/index.js
@@ -5,6 +5,38 @@ myApp.controller('MyController', function() {
   var self = this;
 
   self.headerTitle = "Welcome To Jared's First Homework";
-  self.colors = ['red', 'blue', 'green', 'orange', 'gray', 'none'];
+  self.colorTitle = "Select theme:";
+  self.foldersTitle = "My folders:";
+  self.newItem = {folder: "Personal"} // default value
+
+  self.colors = ['red', 'blue', 'green', 'orange', 'purple', 'none'];
   self.selectedColor = 'none';
+
+  self.folders = [
+  	{ 
+  		name: 'Personal',
+  		items: ['item1','item2']
+  	},
+  	{
+  		name: 'Work',
+  		items: ['item1','item2']
+  	},
+  	{
+  		name: 'Trip',
+  		items: ['item1','item2']
+  	}
+  ];
+
+  self.addNewItem = function(newItemObj){
+  	console.log(newItemObj);
+  	for (var i=0; i<self.folders.length; i++){
+  		var folder = self.folders[i];
+  		if (folder.name === newItemObj.folder){
+			folder.items.push(newItemObj.itemName);
+			self.newItem.itemName = ""; // reset the text input 
+			return;
+  		}
+  	}
+	throw new Error("Folder " + newItemObj.folder + " not found");
+  }
 });
