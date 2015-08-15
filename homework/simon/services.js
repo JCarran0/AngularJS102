6a56f26dd273877eca8bfe45d675c2899cbc8c74
Jared Carrano <jcarrano@newvisions.org>
2015-08-14 22:24:20 -0400

    saving

--- homework/simon/services.js
+++ homework/simon/services.js
@@ -1,38 +1,12 @@
-"use strict";
-
 var MyApp = angular.module('CustomServices', []);
 
-MyApp.factory('GameBuilder', function(){
-	
-	var GameBuilder = function (){
-		this.test = [];
-		/* self.turn can = null, 'simon' or 'user'.
-		** This helps toggle various clickable actions*/
-		this.turn = 'user';
-		
-		this.isClicked = {
-			red: false,
-			blue: false,
-			green: false,
-			yellow: false
-		}		
-	};
+	// mode controls the speed of the game.
+	// i.e. slow, fast, insane.
+	// self.mode = 'fast';
+	// var modeSpeeds = {
+	// 	normal: 800,
+	// 	fast: 500,
+	// 	insane: 200
+	// }
 
-	GameBuilder.prototype.processClick = function(color) {
-		console.log(this.turn)
-		console.log(this.test)
-		if (!this.turn) return;
-		this.currentSequence[this.turn].push(color);
-		flashOneColor(color);
-	};
-
-	function flashOneColor(color){
-		this.isClicked[color] = true;
-		this.currentSequence.display.push(color);
-		return $timeout(function(){
-			this.isClicked[color] = false;
-		}, 500);
-	}	
-
-	return GameBuilder;
-});
+	// 
