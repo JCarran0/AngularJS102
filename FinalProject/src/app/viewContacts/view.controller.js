75bfb695c1ff1e41c5af286cfbbddfc766a3a0b7
Jared Carrano <jcarrano@newvisions.org>
2015-08-27 15:03:06 -0400

    formats selected student obj

--- FinalProject/src/app/viewContacts/view.controller.js
+++ FinalProject/src/app/viewContacts/view.controller.js
@@ -12,17 +12,25 @@
         console.log(students);
         self.students = students;
         self.selectedStudent = students[0];
+        formatStudent();
       })
     }
 
-    // self.oneAtATime = true;
+    function formatStudent(){
+      var stdnt = self.selectedStudent;
+      var ats = stdnt.atsContacts;
+      var home = ats.home;
+      var street = home.street;
+      street = home.apt ? street + ' ' + home.apt : street;
+      stdnt.address = toTitleCase(street + ', ' + home.city + ', NY ' + home.zip);
+      stdnt.phone = home.phone;
+      stdnt.email = home.email;
+    }
 
-    self.addItem = function() {
-      var newItemNo = self.items.length + 1;
-      self.items.push('Item ' + newItemNo);
-    };
+    function toTitleCase(str){
+      return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+    }
 
-    self.isOpen = { home: true };
     self.getStudents();
   });
 
