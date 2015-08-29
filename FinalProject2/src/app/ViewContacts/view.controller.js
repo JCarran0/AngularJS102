2eca71a777c44413fdb481e3e4ee0a162af77000
Jared Carrano <jcarrano@newvisions.org>
2015-08-29 12:37:59 -0400

    adds sub views, but broken adds working version of show/hide editable fields

--- FinalProject2/src/app/ViewContacts/view.controller.js
+++ FinalProject2/src/app/ViewContacts/view.controller.js
@@ -6,27 +6,33 @@
   .controller('MainController', function (StudentNavService) {
     var self = this;
 
+    self.home = {};
+
     StudentNavService.load(function(){
       self.list = StudentNavService.list;
       self.data = StudentNavService.data;
       self.index = StudentNavService.index;
       self.selected = self.data[self.index];
+      formatStudent();
 
       // Why do it this way?
       self.next = function(){
         self.index = StudentNavService.next();
         self.selected = self.data[self.index];
+        formatStudent();
         console.log(self.index)
       }
       self.previous = function(){
         self.index = StudentNavService.previous();
         self.selected = self.data[self.index];
+        formatStudent();
         console.log(self.index)
       }
 
       self.setSelected = function(index){
         self.index = StudentNavService.setIndex(index);
         self.selected = self.data[self.index];
+        formatStudent();
       }
 
       //Instead of this way
@@ -35,6 +41,31 @@
       // self.previous = StudentNavService.previous;
       // self.next = StudentNavService.next;  // George, why doesn't this work?
       // self.previous = StudentNavService.previous;
+
+
+
+      function formatStudent(){
+        var stdnt = self.selected;
+        var ats = stdnt.atsContacts;
+        var home = ats.home;
+        var street = home.street;
+        street = home.apt ? street + ' ' + home.apt : street;
+        stdnt.address = toTitleCase(street + ', ' + home.city + ', NY ' + home.zip);
+        stdnt.phone = home.phone;
+        stdnt.email = home.email;
+        stdnt.ats = ats;
+      }
+
+      function toTitleCase(str){
+        return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+      }
+
+      // Toggle self[field].editActive
+      self.toggleEdit = function(editFlag){
+        console.log('clicked')
+        console.log(typeof editFlag);
+        self[editFlag].editActive = self[editFlag].editActive === true ? false : true;
+      }
     });
 
 
--- FinalProject2/src/app/index.route.js
+++ FinalProject2/src/app/index.route.js
@@ -7,9 +7,24 @@
     $stateProvider
       .state('view', {
         url: '/view',
-        templateUrl: 'app/viewContacts/view.html',
+        templateUrl: 'app/ViewContacts/view.html',
         controller: 'MainController',
         controllerAs: 'ctrl'
+      })
+      .state('logOutreach', {
+        url: '/view/logOutreach',
+        name: 'childViews',
+        templateUrl: 'app/ViewContacts/partials/logOutreach.html'
+      })
+      .state('outreachHistory', {
+        url: '/view/outreachHistory',
+        name: 'childViews',
+        templateUrl: 'app/ViewContacts/partials/outreachHistory.html'
+      })
+      .state('studentData', {
+        url: '/view/studentData',
+        name: 'childViews',
+        templateUrl: 'app/ViewContacts/partials/studentData.html'
       });
 
     $urlRouterProvider.otherwise('/view');
